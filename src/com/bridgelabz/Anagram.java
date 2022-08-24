b52a4a0a85ff1c08cc578c74593f6aeae82b8e92
Prince <princek2530@gmail.com>
2022-08-24 20:52:59 +0530

    Added: Anagram

---
+++ src/com/bridgelabz/Anagram.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+
+    String s1 = "HEART";
+    String s2 = "EARTH";
+    char c1[] = s1.toCharArray();
+    char c2[] = s2.toCharArray();
+    if(c1.length != c2.length){
+        System.out.println("Not Anagram");
+        System.exit(0);
+    }
+    Arrays.sort(c1);
+    Arrays.sort(c2);
+    for(int i=0; i< c1.length; i++){
+        if (c1[i] !=c2[i]){
+            System.out.println("Not Anagram");
+            System.exit(0);
+        }
+    }
+        System.out.println("It is Anagram");
+
+}}
